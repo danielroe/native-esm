72a707fd2ed67cbe059df14d20f6cd32e85ce85c
Daniel Roe <daniel@roe.dev>
2021-10-27 13:20:50 +0100

    chore: add esm module example

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import esm from 'esm'
 import cjs from 'cjs'
 import jsCjs from 'js-cjs'
+import esmModule from 'esm-module'
 // import jsEsm from 'js-esm'
 
-console.log({ esm, cjs, jsCjs, /* jsEsm */ })
+console.log({ esm, cjs, jsCjs, esmModule, /* jsEsm */ })
