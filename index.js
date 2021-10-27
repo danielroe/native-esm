523dbfea70e3dee3687b5dff33d893c9793564ab
Daniel Roe <daniel@roe.dev>
2021-10-27 13:47:07 +0100

    feat: example of what works _outside_ native esm

---
+++ index.js
@@ -0,0 +1,7 @@
+// const esm = require('esm')
+const cjs = require('cjs')
+const jsCjs = require('js-cjs')
+// const esmModule = require('esm-module')
+// const jsEsm = require('js-esm')
+
+console.log({ /* esm, */ cjs, jsCjs, /* esmModule, jsEsm */ })
