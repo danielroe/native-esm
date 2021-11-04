75d30c0ff1060d1c5f6c09f221c48e281454e564
Daniel Roe <daniel@roe.dev>
2021-11-04 12:16:44 +0000

    feat: add named examples

---
+++ index-named.js
@@ -0,0 +1,7 @@
+// const esm = require('esm-named')
+const cjs = require('cjs-named')
+const jsCjs = require('js-cjs-named')
+// const esmModule = require('esm-module-named')
+// const jsEsm = require('js-esm-named')
+
+console.log({ /* esm, */ cjs, jsCjs, /* esmModule, jsEsm */ })
---
+++ index-named.mjs
@@ -0,0 +1,7 @@
+import { esm } from 'esm-named'
+import { cjs } from 'cjs-named'
+import { jsCjs } from 'js-cjs-named'
+import { esmModule } from 'esm-module-named'
+// import { jsEsm } from 'js-esm-named'
+
+console.log({ esm, cjs, jsCjs, esmModule, /* jsEsm */ })
